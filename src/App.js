dd839eb997bed2edc9f8482dc459f247071b8f5d
tahmit <tahmit@uw.edu>
2020-02-04 12:59:59 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-function App() {
+function App(props) {
+  const [text, setText] = useState('')
+  const [memes, setMemes] = useState([])
+
+  async function getMemes() {
+    const api_key = 'sNxa1otQT8ndji7kzM8XWzSxvzo88N3X'
+    let url = 'https://api.giphy.com/v1/gifs/search?'
+    url += 'api_key=' + api_key
+    url +='&q=' + text
+    const r = await fetch(url)
+    const body = await r.json()
+    setMemes(body.data)
+    setText('')
+  }
+
+  console.log(memes)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="input-wrap">
+          <TextField 
+            fullWidth
+            label="Search your favorite dank memes!" variant="filled" 
+            value={text}
+            onChange={e => setText(e.target.value)}
+            onKeyPress = {e => {
+              if (e.key === 'Enter') getMemes()
+            }}
+          />
+          <Button variant="contained" color="primary" onClick={getMemes}>
+            SEARCH
+          </Button>
+        </div>
+        <div className="meme-wrap">
+
+        </div>
       </header>
     </div>
   );
+}
+
+function Meme({title, images}) {
+  return <div className="memes">
+    <img src={images.fixed_height.url} alt="meme"/>
+    <div className="meme-title"></div>
+  </div>
 }
 
 export default App;
