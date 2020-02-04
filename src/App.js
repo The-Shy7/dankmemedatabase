ec19dd8648fda69180f7e5a703932eb2cbc37ff3
tahmit <tahmit@uw.edu>
2020-02-04 13:07:42 -0800

    Imported Material UI components

--- src/App.js
+++ src/App.js
@@ -2,54 +2,38 @@ import React, {useState} from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { LinearProgress } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
 
-function App(props) {
+function App() {
   const [text, setText] = useState('')
-  const [memes, setMemes] = useState([])
-
-  async function getMemes() {
-    const api_key = 'sNxa1otQT8ndji7kzM8XWzSxvzo88N3X'
-    let url = 'https://api.giphy.com/v1/gifs/search?'
-    url += 'api_key=' + api_key
-    url +='&q=' + text
-    const r = await fetch(url)
-    const body = await r.json()
-    setMemes(body.data)
-    setText('')
-  }
-
-  console.log(memes)
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="input-wrap">
-          <TextField 
-            fullWidth
-            label="Search your favorite dank memes!" variant="filled" 
+          <TextField fullWidth variant="outlined"
+            label="Search for memes!"
             value={text}
-            onChange={e => setText(e.target.value)}
-            onKeyPress = {e => {
-              if (e.key === 'Enter') getMemes()
+            onChange={e=> setText(e.target.value)}
+            onKeyPress={e=> {
+              if(e.key==='Enter') getMemes()
             }}
           />
-          <Button variant="contained" color="primary" onClick={getMemes}>
-            SEARCH
+          <Button variant="contained" color="primary"
+            onClick={getMemes}>
+            <Search />
+            Search
           </Button>
         </div>
-        <div className="meme-wrap">
-
-        </div>
       </header>
+      {loading && <LinearProgress />}
+
+      <div className="memes">
+        {memes.map((meme,i)=> <Meme key={i} {...meme} />)}
+      </div>
     </div>
   );
-}
-
-function Meme({title, images}) {
-  return <div className="memes">
-    <img src={images.fixed_height.url} alt="meme"/>
-    <div className="meme-title"></div>
-  </div>
 }
 
 export default App;
